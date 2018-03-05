2d3647115ab7a0e8ed2e105afebf8b2eb3fc8c5a
Xavier Arques <xavierarques@yahoo.com>
2018-03-05 22:40:40 +0100

    Code review feedback: Since the component is not managing state it should be a stateless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,31 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Shelf from './Shelf';
 
-class ListBooks extends Component {
-  static propTypes = {
-    currentlyReading: PropTypes.array.isRequired,
-    wantToRead: PropTypes.array.isRequired,
-    read: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired
-  }
-  
-  render() {
-    const { currentlyReading, wantToRead, read, changeShelf } = this.props;
-    return (
-      <div className='my-reads'>
-        <div className='my-reads-title'>
-          <h1>My Reads</h1>
-        </div>
-        <div className='shelves'>
-          <Shelf title='Currently Reading' books={currentlyReading} changeShelf={ changeShelf }/>
-          <Shelf title='Want to read' books={wantToRead} changeShelf={changeShelf}/>
-          <Shelf title='Read' books={read} changeShelf={changeShelf}/>
-        </div>
-        <Link className='search-link' to='/search'/>
+const ListBooks = (props) => {
+  const { currentlyReading, wantToRead, read, changeShelf } = props;
+  return (
+    <div className='my-reads'>
+      <div className='my-reads-title'>
+        <h1>My Reads</h1>
       </div>
-    )
-  }
+      <div className='shelves'>
+        <Shelf title='Currently Reading' books={currentlyReading} changeShelf={ changeShelf }/>
+        <Shelf title='Want to Read' books={wantToRead} changeShelf={changeShelf}/>
+        <Shelf title='Read' books={read} changeShelf={changeShelf}/>
+      </div>
+      <Link className='search-link' to='/search'/>
+    </div>
+  )
 }
+
+ListBooks.propTypes = {
+  currentlyReading: PropTypes.array.isRequired,
+  wantToRead: PropTypes.array.isRequired,
+  read: PropTypes.array.isRequired,
+  changeShelf: PropTypes.func.isRequired
+}
+
 export default ListBooks;
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ShelfMenu from './ShelfMenu';
 
-class Shelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    title: PropTypes.string,
-    changeShelf: PropTypes.func.isRequired
-  }
-
-  render() {
-    const { books, title, changeShelf } = this.props;
-    return (
-      <div className='book-shelf'>
-        {title && <h2 className='book-shelf-title'>{title}</h2> }
-        <ul className='book-list'>
-          { books.map(book => 
-              <li className='book-list-item' key={book.id}>
-                <div className='book-cover' style={{
+const Shelf = (props) =>  {
+  const { books, title, changeShelf } = props;
+  return (
+    <div className='book-shelf'>
+      {title && <h2 className='book-shelf-title'>{title}</h2> }
+      <ul className='book-list'>
+        { books.map(book => 
+            <li className='book-list-item' key={book.id}>
+              <div className='book-cover' style={{
                 backgroundImage: `url(${(book.imageLinks && book.imageLinks.smallThumbnail) || 'icons/no-image-available.jpg' })`
-                }} />
-                <ShelfMenu book={ book } changeShelf= { changeShelf }/>
-                <p className='book-title'>{book.title}</p>
-                <ul className='author-list'>
-                  { book.authors && book.authors.map(author => <li key={author}>{author}</li>) }
-                </ul>
-              </li>
-            )
-          }
-        </ul>
-      </div>
-    )
-  }
+              }} />
+              <ShelfMenu book={ book } changeShelf= { changeShelf }/>
+              <p className='book-title'>{book.title}</p>
+              <ul className='author-list'>
+                { book.authors && book.authors.map(author => <li key={author}>{author}</li>) }
+              </ul>
+            </li>
+          )
+        }
+      </ul>
+    </div>
+  )
+}
+
+Shelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  title: PropTypes.string,
+  changeShelf: PropTypes.func.isRequired
 }
 
 export default Shelf;
